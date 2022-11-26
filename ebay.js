ec310b8caf997cea085d7a65ae097f31ed18bbd8
tariq <kapooky101@gmail.com>
2022-11-25 20:38:31 -0500

    init

--- ebay.js
+++ ebay.js
@@ -1,6 +1,7 @@
 const sqlite3 = require('sqlite3');
 const eBayApi = require('ebay-api');
 const fs = require("fs");
+const tokenobject = require("./data.json"); 
 
 function jsonReader(filePath, cb) {
   fs.readFile(filePath, (err, fileData) => {
@@ -14,6 +15,20 @@ function jsonReader(filePath, cb) {
       return cb && cb(err);
     }
   });
+}
+
+function updateTokenJSON(token) {
+  jsonReader("./data.json",(err,json) => {
+    if(err){
+      console.log(err);
+      return; 
+    }
+    // json.j
+    //Update the token 
+    const jsonString = JSON.stringify(customer);
+    fs.writeFileSync('./data.json', jsonString)
+  })
+  
 }
 
 
@@ -49,6 +64,7 @@ const eBay = new eBayApi({
   console.log('Open URL', url);
   const token = await eBay.OAuth2.getToken("v^1.1#i^1#r^1#f^0#p^3#I^3#t^Ul41Xzc6OUM5MDY4NUU1NURERkFFNTM5NTM4MzA5NkNFOTlFMTBfMl8xI0VeMjYw");
   console.log(token);
+  console.log(JSON.stringify(token));
   eBay.OAuth2.setCredentials(token);
   // eBay.OAuth2.mintApplicationAccessToken(); 
   // let code = await eBay.OAuth2.getToken("v%5E1.1%23i%5E1%23r%5E1%23p%5E3%23f%5E0%23I%5E3%23t%5EUl41Xzc6OEIxNDVGMjJGMkIwQjVDM0Q3MDNBMDAzRTY2QUJFQUFfMF8xI0VeMjYw")
