e3896832f1d9c3de27599b64b341d2940b628ffd
tariq <kapooky101@gmail.com>
2023-11-09 10:57:47 +0500

    Updated to add MW3 4 hour codes

--- getCodesManually.js
+++ getCodesManually.js
@@ -4,7 +4,7 @@ import  {fetchcodes} from "./dynamodb.js";
 (async () => {
     //let codes = await fetchcodes(12,"myself","5hour2xp10codes")
     
-    let codes = await fetchcodes(1,"myself","codes")
+    let codes = await fetchcodes(3,"myself","jacklinks")
     for(let x = 0; x < codes.codes.length; x++){
       // console.log(`${x+1}. ${codes.codes[x].toString().replace(/-/g,"")} ${codes.links[x].toString()}`);
        console.log(`${codes.codes[x].toString().replace(/-/g,"-")}`);
--- index.ts
+++ index.ts
@@ -42,6 +42,16 @@ let listings: Listing[] = [{
         Instructions: "Redeem at https://callofduty.com/bkredeem"
     },
     {
+        quantityMultiplier: 4,
+        legacyItemID: ['276157292677'],
+        Description: "4-hour BK MW3 Codes",
+        DBtable: Index.codes,
+        Game: Game.COD,
+        ListingType: ListingType.CODE,
+        Subject: "✅Here's your 4-hour 2XP codes!",
+        Instructions: "Redeem at https://callofduty.com/bkredeem"
+    },
+    {
         quantityMultiplier: 1,
         legacyItemID: ['225471847770'],
         Description: "jacklinks 1 code only",
